ec51470473121af90e1e3026a5f5409e6c794761
Thomas Brunbjerg <thomb@hotmail.dk>
2020-07-05 10:18:10 +0200

    Add wiki links to correct answer dialogs

---
+++ DynamicPotterTrivia/Models/LotrQuote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicPotterTrivia.Models
+{
+    public class LotrQuote
+    {
+        public string Id { get; set; }
+        public string Dialog { get; set; }
+        public string Movie { get; set; }
+        public string Character { get; set; }
+    }
+}
