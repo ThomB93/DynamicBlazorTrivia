fb335ef9bfa38a030aca7daad67f5e087c9b25a9
Thomas Brunbjerg <thomb@hotmail.dk>
2020-07-06 15:52:50 +0200

    add radar charts to statistics page

--- DynamicPotterTrivia/Pages/Statistics.razor.cs
+++ DynamicPotterTrivia/Pages/Statistics.razor.cs
@@ -13,27 +13,32 @@ using ChartJs.Blazor.ChartJS.PieChart;
 using ChartJs.Blazor.Charts;
 using ChartJs.Blazor.Util;
 using ChartJs.Blazor.ChartJS.Common.Wrappers;
+using ChartJs.Blazor.ChartJS.RadarChart;
 
 namespace DynamicPotterTrivia.Pages
 {
     public partial class Statistics
     {
-        private PieConfig _PieConfig;
-        private ChartJsPieChart _pieChartJs;
+        private PieConfig _PieConfigScoreByCategory;
+        private ChartJsPieChart _pieChartJsScoreByCategory;
         private BarConfig _BarConfig;
         private ChartJsBarChart _barChartJs;
+        private RadarConfig _RadarConfigHP;
+        private ChartJsRadarChart _radarChartJsHP;
+        private RadarConfig _RadarConfigLOTR;
+        private ChartJsRadarChart _radarChartJsLOTR;
 
         protected override void OnInitialized()
         {
             //Add Pie Chart with scores by category
-            _PieConfig = new PieConfig
+            _PieConfigScoreByCategory = new PieConfig
             {
                 Options = new PieOptions
                 {
                     Title = new OptionsTitle
                     {
                         Display = true,
-                        Text = "Total Score by Category"
+                        Text = "Score by Category"
                     },
                     Responsive = true,
                     Animation = new ArcAnimation
@@ -44,7 +49,7 @@ namespace DynamicPotterTrivia.Pages
                 }
             };
 
-            _PieConfig.Data.Labels.AddRange(new[] { "Harry Potter", "Lord of the Rings" });
+            _PieConfigScoreByCategory.Data.Labels.AddRange(new[] { "Harry Potter", "Lord of the Rings" });
 
             var pieSet = new PieDataset
             {
@@ -53,11 +58,11 @@ namespace DynamicPotterTrivia.Pages
                 //HoverBackgroundColor = ColorUtil.RandomColorString(),
                 //HoverBorderColor = ColorUtil.RandomColorString(),
                 //HoverBorderWidth = 1,
-                BorderColor = "#000000",
+                BorderColor = "#000000"
             };
 
             pieSet.Data.AddRange(new double[] { ScoreTrackerService.GetTotalHPScore(), ScoreTrackerService.GetTotalLOTRScore()});
-            _PieConfig.Data.Datasets.Add(pieSet);
+            _PieConfigScoreByCategory.Data.Datasets.Add(pieSet);
             //end of pie chart config
 
             //bar chart config
@@ -68,7 +73,7 @@ namespace DynamicPotterTrivia.Pages
                     Title = new OptionsTitle
                     {
                         Display = true,
-                        Text = "Correct vs. Wrong Answers"
+                        Text = "Hints & Clues"
                     },
                     Scales = new BarScales
                     {
@@ -99,21 +104,85 @@ namespace DynamicPotterTrivia.Pages
                     }
                 }
             };
-            _BarConfig.Data.Labels.AddRange(new [] {"Correct Answers", "Wrong Answers"});
+            _BarConfig.Data.Labels.AddRange(new [] {"HP Hints Used", "LOTR Hints Used", "HP Clues Used", "LOTR Clues Used"});
             var barDataset = new BarDataset<Int32Wrapper>
             {
-                BackgroundColor = new[] {ColorUtil.RandomColorString(), ColorUtil.RandomColorString() },
+                BackgroundColor = new[] {ColorUtil.RandomColorString(), ColorUtil.RandomColorString(), ColorUtil.RandomColorString(), ColorUtil.RandomColorString() },
                 BorderWidth = 1,
                 //HoverBackgroundColor = ColorUtil.RandomColorString(),
                 //HoverBorderColor = ColorUtil.RandomColorString(),
                 //HoverBorderWidth = 1,
                 BorderColor = "#000000",
-
+                Label = "Use of Hints & Clues"
             };
 
-            barDataset.AddRange(new Int32Wrapper[] {ScoreTrackerService.GetTotalCorrectAnswers(), ScoreTrackerService.GetTotalWrongAnswers()});
+            barDataset.AddRange(new Int32Wrapper[] {ScoreTrackerService.GetHPHintsUsed(), ScoreTrackerService.GetLOTRHintsUsed(), ScoreTrackerService.GetHPCluesUsed(), ScoreTrackerService.GetLOTRCluesUsed()});
             _BarConfig.Data.Datasets.Add(barDataset);
             //end of bar chart config
+
+            //start of HP radar config
+            _RadarConfigHP = new RadarConfig
+            {
+                Options = new RadarOptions
+                {
+                    Title = new OptionsTitle
+                    {
+                        Display = true,
+                        Text = "Harry Potter Data"
+                    },
+                    Responsive = true
+                }
+            };
+            _RadarConfigHP.Data.Labels.AddRange(new string[] { "Score", "Hints Used", "Clues Used", "Correct Answers", "Wrong Answers" });
+            RadarDataset dataset_hp = new RadarDataset
+            {
+                //Label = $"Participant {_RadarConfigHP.Data.Datasets.Count + 1}",
+                BorderColor = ColorUtil.RandomColorString(),
+                BorderWidth = 1,
+                Data = new List<double>()
+            };
+            double[] radarDataHP = new double[5]
+            {
+                ScoreTrackerService.GetTotalHPScore(), ScoreTrackerService.GetHPHintsUsed(),
+                ScoreTrackerService.GetHPCluesUsed(), ScoreTrackerService.GetHPCorrectAnswers(),
+                ScoreTrackerService.GetHPWrongAnswers()
+            };
+
+            dataset_hp.Data.AddRange(radarDataHP);
+            _RadarConfigHP.Data.Datasets.Add(dataset_hp);
+            //end of HP radar config
+
+            //start of LOTR radar config
+            _RadarConfigLOTR = new RadarConfig
+            {
+                Options = new RadarOptions
+                {
+                    Title = new OptionsTitle
+                    {
+                        Display = true,
+                        Text = "Lord of the Rings Data"
+                    },
+                    Responsive = true
+                }
+            };
+            _RadarConfigLOTR.Data.Labels.AddRange(new string[] { "Score", "Hints Used", "Clues Used", "Correct Answers", "Wrong Answers" });
+            RadarDataset dataset_lotr = new RadarDataset
+            {
+                //Label = $"Participant {_RadarConfigHP.Data.Datasets.Count + 1}",
+                BorderColor = ColorUtil.RandomColorString(),
+                BorderWidth = 1,
+                Data = new List<double>()
+            };
+            double[] radarDataLOTR = new double[5]
+            {
+                ScoreTrackerService.GetTotalLOTRScore(), ScoreTrackerService.GetLOTRHintsUsed(),
+                ScoreTrackerService.GetLOTRCluesUsed(), ScoreTrackerService.GetLOTRCorrectAnswers(),
+                ScoreTrackerService.GetLOTRWrongAnswers()
+            };
+
+            dataset_lotr.Data.AddRange(radarDataLOTR);
+            _RadarConfigLOTR.Data.Datasets.Add(dataset_lotr);
+            //end of HP radar config
         }
     }
 }
